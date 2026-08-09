1bb2e3596d2b6259876caf318f4e8178fbcc93ee
Dave Elder <7348647+DrGr4f1x@users.noreply.github.com>
2026-08-08 20:31:25 -0700

    Starting ROCm version

---
+++ OneWeekend/Chapter03/Ray.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Vec3.h"
+
+class Ray
+{
+public:
+	__device__ Ray() {}
+	__device__ Ray(const Vec3& a, const Vec3& b) : A{ a }, B{ b } {}
+	__device__ Vec3 Origin() const { return A; }
+	__device__ Vec3 Direction() const { return B; }
+	__device__ Vec3 PointAtParameter(float t) const { return A + t * B; }
+
+	Vec3 A;
+	Vec3 B;
+};
